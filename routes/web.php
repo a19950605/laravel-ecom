6640ed7c6b421b6223dc984eaec783c6911acef4
wuchau <wuchau@connect.ust.hk>
2021-01-26 19:43:43 +0800

    orders

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,8 @@ Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::get("search",[ProductController::class,'search']);
 Route::post("add_to_cart",[ProductController::class,'addToCart']);
 Route::get("cartlist",[ProductController::class,'cartList']);
+Route::get("removecart/{id}",[ProductController::class,'removeCart']);
+Route::get("ordernow",[ProductController::class,'orderNow']);
+Route::post("orderplace",[ProductController::class,'orderPlace']);
+Route::post("ordernow",[ProductController::class,'orderPlace']);
+Router::get("myorders",[ProductController::class,'myOrders']);
